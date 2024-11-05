fb9867937171d92757ff3454062d8059f8c8caf9
fdebrus <33791533+fdebrus@users.noreply.github.com>
2024-11-05 14:50:52 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     ],
     classifiers=[
         'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',  # Update if using a different license
+        'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
     python_requires='>=3.7',
