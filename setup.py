63bdd4e60bbd3f3551557432b04ca2959e1c6be3
fdebrus <33791533+fdebrus@users.noreply.github.com>
2024-11-05 13:39:18 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setup(
+    name='nikobusconnect',
+    version='0.1.0',
+    author='Frederic Debrus',
+    author_email='fdebrus@hotmail.com',
+    description='A Python library for connecting to Nikobus systems',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/fdebrus/nikobusconnect',
+    packages=find_packages(),
+    install_requires=[
+        'pyserial-asyncio>=0.5',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',  # Update if using a different license
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.7',
+)
