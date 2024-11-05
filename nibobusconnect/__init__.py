80344166e3d36019458ef9d40b2a430e203b5403
fdebrus <33791533+fdebrus@users.noreply.github.com>
2024-11-05 13:51:59 +0100

    Update __init__.py

--- nibobusconnect/__init__.py
+++ nibobusconnect/__init__.py
@@ -1,6 +1,7 @@
 # nikobusconnect/__init__.py
 
 from .connection import NikobusConnect
+from .command_handler import NikobusCommandHandler
 from .protocol import (
     int_to_hex,
     calc_crc1,
@@ -10,9 +11,11 @@ from .protocol import (
     make_pc_link_command,
     calculate_group_number,
 )
+from .message_parser import parse_message
 
 __all__ = [
     'NikobusConnect',
+    'NikobusCommandHandler',
     'int_to_hex',
     'calc_crc1',
     'calc_crc2',
@@ -20,6 +23,7 @@ __all__ = [
     'append_crc2',
     'make_pc_link_command',
     'calculate_group_number',
+    'parse_message',
 ]
 
 __version__ = '0.1.0'
