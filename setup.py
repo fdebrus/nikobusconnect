c40ba53f6216ebb5640ca9ad97cb9c5b9292609c
fdebrus <33791533+fdebrus@users.noreply.github.com>
2024-11-06 14:12:43 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md', 'r') as fh:
 
 setup(
     name='nikobusconnect',
-    version='0.1.0',
+    version='0.1.1',
     author='Frederic Debrus',
     author_email='fdebrus@hotmail.com',
     description='A Python library for connecting to Nikobus systems',
