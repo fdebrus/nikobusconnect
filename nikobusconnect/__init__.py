170b0898e3d9b6f3e127910e02ae51da4d8a5c5a
fdebrus <33791533+fdebrus@users.noreply.github.com>
2026-02-22 23:29:47 +0100

    Refactor imports and update version to 1.0.0

--- nikobusconnect/__init__.py
+++ nikobusconnect/__init__.py
@@ -1,29 +1,29 @@
-# nikobusconnect/__init__.py
+"""
+NikobusConnect: asynchronous library for Nikobus.
+"""
 
+from .api import NikobusAPI
+from .command import NikobusCommandHandler
 from .connection import NikobusConnect
-from .command_handler import NikobusCommandHandler
+from .listener import NikobusEventListener
 from .protocol import (
-    int_to_hex,
     calc_crc1,
     calc_crc2,
-    append_crc1,
-    append_crc2,
-    make_pc_link_command,
     calculate_group_number,
+    int_to_hex,
+    make_pc_link_command,
 )
-from .message_parser import parse_message
 
 __all__ = [
-    'NikobusConnect',
-    'NikobusCommandHandler',
-    'int_to_hex',
-    'calc_crc1',
-    'calc_crc2',
-    'append_crc1',
-    'append_crc2',
-    'make_pc_link_command',
-    'calculate_group_number',
-    'parse_message',
+    "NikobusAPI",
+    "NikobusCommandHandler",
+    "NikobusConnect",
+    "NikobusEventListener",
+    "calc_crc1",
+    "calc_crc2",
+    "calculate_group_number",
+    "int_to_hex",
+    "make_pc_link_command",
 ]
 
-__version__ = '0.1.0'
+__version__ = "1.0.0"  # Jump to 1.0.0 for the Platinum Release
